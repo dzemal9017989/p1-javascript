098b5bc268f869607eee686833b72f9442480438
dzemal9017989 <9017989@student.zadkine.nl>
2022-10-14 12:32:28 +0200

    11

---
+++ DOM FOLDER/script.js
@@ -0,0 +1,28 @@
+alert("Hello world");
+
+// kleur aanpassen
+document.getElementById("title").style.color = red;
+
+//variabelen gebruiken
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert(getal1 + '' x '' + " = " + som);
+
+
+// propmtbox
+var kleur = propmt ("Geef een Engelse kleurnaam: (red of blue", "");
+
+// kleur aanapssen
+document.getElementById("title").style.color = kleur;
+//document.get.ElementById("title").style.color = "red";
+
+//tekst aanpassen
+document.getElementById("p1").innerHTML = " Hello "+ kleur +"!";
+
+
+/*
+
+var kleur = prompt ('Geef een Engelse kleurnaam':(red of blue', '''');
+
+*/
