61de93217a23f40d6a7c55b9316e19abb2572f6b
dzemal9017989 <9017989@student.zadkine.nl>
2023-01-04 17:29:17 +0100

    js opdracht 8.1

---
+++ opdracht 8.1/app.js
@@ -0,0 +1,26 @@
+function multiply(){
+    let x = 2;
+    let y = 2;
+    let z = x * y;
+    document.getElementById("answer").innerHTML = "Antwoord is:" + 4;
+}
+function minus(){
+    let x = 10;
+    let y = 5;
+    let z = x - y;
+    document.getElementById("answer").innerHTML = "Antwoord is:" + 5;
+}
+
+function add(){
+    let x = 15;
+    let y = 15;
+    let z = x + y;
+    document.getElementById("answer").innerHTML = "Antwoord is:" + 30;
+}
+
+function devide(){
+    let x = 14;
+    let y = 2;
+    let z = x / y;
+    document.getElementById("answer").innerHTML = "Antwoord is:" + 7;
+}
